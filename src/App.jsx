51143336abcc4f9df4ccf6f9089d41eed7a6c67b
pramesywaraj <pramesywaraj@pahamify.com>
2022-08-14 21:58:34 +0700

    chore: remove unused style or jsx code
    remove unused code in App.jsx, also remove generated style file

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,11 @@
-import reactLogo from './assets/react.svg'
-import './App.css'
+import style from '@/App.module.css';
 
 function App() {
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React 2</h1>
-      <div className="card">
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div className={style.testing}>
+      <h1>EFishery Test App</h1>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
