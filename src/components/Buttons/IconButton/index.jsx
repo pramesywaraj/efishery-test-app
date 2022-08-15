f352b9c7c55c79abe6cd6ed83509a52d934a3d5e
pramesywaraj <pramesywaraj@pahamify.com>
2022-08-16 00:52:08 +0700

    feat: add icon button component
    add icon button component that support svg as it's icon

---
+++ src/components/Buttons/IconButton/index.jsx
@@ -0,0 +1,17 @@
+import { checkClassName } from '@helpers/style';
+import styles from './styles.module.scss';
+
+const IconButton = ({ onClick, className, iconClassName, Icon }) => {
+  const buttonClasses = checkClassName(styles['icon-btn'], className);
+  const iconClasses = checkClassName(styles['icon-image'], iconClassName);
+  return (
+    <button
+      className={buttonClasses}
+      onClick={onClick}
+    >
+      <Icon className={iconClasses} />
+    </button>
+  );
+};
+
+export default IconButton;
