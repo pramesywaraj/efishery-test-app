8033f359cf807f7a023973de7bcea7bd3d8c17c8
pramesywaraj <pramesywaraj@pahamify.com>
2022-08-16 00:57:17 +0700

    feat: add search input component

--- src/pages/HomePage/partials/ItemListSection/index.jsx
+++ src/pages/HomePage/partials/ItemListSection/index.jsx
@@ -1,9 +1,27 @@
+import { useState } from 'react';
+
+import Search from '@components/Search';
 import styles from './styles.module.scss';
 
 const ItemListSection = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+  
+  const onChangeSearchText = (event) => {
+    const { value } = event.target;
+    event.preventDefault();
+
+    setSearchTerm(value);
+  };
+  
   return (
-    <section>
-      Item List
+    <section className={styles['itemlist-section']}>
+      <Search onChange={onChangeSearchText} searchTerm={searchTerm} />
+      <div>
+        {searchTerm}
+      </div>
+      <div>
+        item list
+      </div>
     </section>
   );
 };
