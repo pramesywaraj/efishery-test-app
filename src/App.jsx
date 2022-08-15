10d47a607fba8e91523f676ada1f0ba9aa989199
pramesywaraj <pramesywaraj@pahamify.com>
2022-08-15 13:41:27 +0700

    feat: add screen layout
    add screen layout and change routes pattern

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,16 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import HomePage from '@pages/HomePage';
 import InputPage from '@pages/InputPage';
 
+import ScreenLayout from '@layouts/ScreenLayout';
+
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/input-item" element={<InputPage />} />
+        <Route path="/" element={<ScreenLayout />}>
+          <Route index element={<HomePage />} />
+          <Route path="input-item" element={<InputPage />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
