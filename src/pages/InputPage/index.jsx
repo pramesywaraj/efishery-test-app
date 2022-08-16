3780b598c295584010f9ea107e30e1c0fc0ce0af
pramesywaraj <pramesywaraj@pahamify.com>
2022-08-16 22:20:08 +0700

    feat: add form to input page

--- src/pages/InputPage/index.jsx
+++ src/pages/InputPage/index.jsx
@@ -1,8 +1,39 @@
+import JsonToForm from 'json-reactform';
+import styles from './styles.module.scss';
+
 const InputPage = () => {
+  const formSchema = {
+    "Nama Komoditas": {
+      "type": "text",
+      "required": true
+    },
+    "Kota": {
+      "type": "text",
+      "required": true
+    },
+    "Provinsi": {
+      "type": "text",
+      "required": true
+    },
+    "Besar": {
+      "type": "number",
+      "required": true
+    },
+    "Harga per kg (dalam rupiah)": {
+      "type": "currency",
+      "required": true
+    },
+    "Save": { // button submit
+      "type": "submit",
+    }
+  };
   return (
-    <div>
-      <h1>Hello this is Input Page</h1>
-    </div>
+    <main className={styles['inputpage-main']}>
+      <header>
+        <h2>Tambah Produk Baru</h2>
+      </header>
+      <JsonToForm model={formSchema} onSubmit={(params) => console.log('AHAi', params)} />
+    </main>
   );
 };
 
