f8a9d208d0027bb0381cb939db2658b897993b09
pramesywaraj <pramesywaraj@pahamify.com>
2022-08-16 16:31:16 +0700

    feat: add item list layout switcher
    add product item list layout switch functionality, to change from
    grid view into list view or vice-versa

--- src/components/Screens/HomePage/LayoutSwitch/index.jsx
+++ src/components/Screens/HomePage/LayoutSwitch/index.jsx
@@ -2,23 +2,24 @@ import { IconButton } from '@components/Buttons';
 
 import { ReactComponent as IconGrid } from '@assets/images/icons/icon-grid.svg';
 import { ReactComponent as IconList } from '@assets/images/icons/icon-list.svg';
+import { PRODUCT_ITEMS_LAYOUT_GRID, PRODUCT_ITEMS_LAYOUT_LIST } from '@constants/product';
 
 import styles from './styles.module.scss';
 
-const LayoutSwitch = () => {
+const LayoutSwitch = ({ onChangeLayout, renderedLayout }) => {
   return (
     <div className={styles['itemlist-subheader-switch']}>
       <IconButton
-        onClick={() => {}}
+        onClick={() => onChangeLayout(PRODUCT_ITEMS_LAYOUT_GRID)}
         alt="Grid"
-        className={`${styles['itemlist-switch-button']} ${styles['active']}`}
+        className={`${styles['itemlist-switch-button']} ${renderedLayout === PRODUCT_ITEMS_LAYOUT_GRID && styles['active']}`}
         iconClassName={styles['icon']}
         Icon={IconGrid}
       />
       <IconButton
-        onClick={() => {}}
+        onClick={() => onChangeLayout(PRODUCT_ITEMS_LAYOUT_LIST)}
         alt="List"
-        className={`${styles['itemlist-switch-button']}`}
+        className={`${styles['itemlist-switch-button']} ${renderedLayout === PRODUCT_ITEMS_LAYOUT_LIST && styles['active']}`}
         iconClassName={styles['icon']}
         Icon={IconList}
       />
--- src/pages/HomePage/partials/ItemListSection/index.jsx
+++ src/pages/HomePage/partials/ItemListSection/index.jsx
@@ -1,10 +1,16 @@
 import { useState } from 'react';
 
 import Search from '@components/Search';
+import { GridProductItem } from '@components/Items';
+import LayoutSwitch from '@components/Screens/HomePage/LayoutSwitch';
+
+import { PRODUCT_ITEMS_LAYOUT_GRID } from '@constants/product';
+
 import styles from './styles.module.scss';
 
 const ItemListSection = () => {
   const [searchTerm, setSearchTerm] = useState('');
+  const [itemsLayout, setItemsLayout] = useState(PRODUCT_ITEMS_LAYOUT_GRID);
   
   const onChangeSearchText = (event) => {
     const { value } = event.target;
@@ -17,14 +23,20 @@ const ItemListSection = () => {
     // handle search here based on search term
   };
   
+  const onChangeLayout = (selectedLayout) => {
+    setItemsLayout(selectedLayout);
+  };
+  
   return (
     <section className={styles['itemlist-section']}>
       <Search onChange={onChangeSearchText} searchTerm={searchTerm} onSubmit={onSubmitSearch} />
-      <div>
-        {searchTerm}
+      <div className={styles['itemlist-subheader-container']}>
+        <p className={styles['text-result']}>Menampilkan 300 hasil</p>
+        <LayoutSwitch onChangeLayout={onChangeLayout} renderedLayout={itemsLayout} />
       </div>
-      <div>
-        item list
+      <div className={`${styles['itemlist-container']} ${itemsLayout === PRODUCT_ITEMS_LAYOUT_GRID ? styles['grid-view'] : styles['list-view']}`}>
+        <GridProductItem />
+        <GridProductItem />
       </div>
     </section>
   );
