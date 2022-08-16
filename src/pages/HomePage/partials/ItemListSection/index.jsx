44c6bff02ad9f201e05fb0163dfb029cd20bfd6d
pramesywaraj <pramesywaraj@pahamify.com>
2022-08-16 20:03:59 +0700

    feat: handle loading fetch data
    handle loading state when app fetching the data, user will see loading
    placeholder or skeleton upon waiting the data to be fetched

--- src/pages/HomePage/partials/ItemListSection/index.jsx
+++ src/pages/HomePage/partials/ItemListSection/index.jsx
@@ -5,15 +5,21 @@ import Search from '@components/Search';
 import { GridProductItem } from '@components/Items';
 import LayoutSwitch from '@components/Screens/HomePage/LayoutSwitch';
 import { FloatingButton } from '@components/Buttons';
+import { LoadingPlaceholder } from '@components/Loaders';
+
+import useFetch from '@hooks/useFetch';
 
 import { PRODUCT_ITEMS_LAYOUT_GRID } from '@constants/product';
 import { ReactComponent as IconAdd } from '@assets/images/icons/icon-add.svg';
 
 import styles from './styles.module.scss';
 
+const DUMMY_LOADING_ARRAY = [1, 2, 3, 4, 5, 6, 7, 8];
+
 const ItemListSection = () => {
   const [searchTerm, setSearchTerm] = useState('');
   const [itemsLayout, setItemsLayout] = useState(PRODUCT_ITEMS_LAYOUT_GRID);
+  const { isLoading, data } = useFetch('/list');
   
   const navigation = useNavigate();
   
@@ -44,7 +50,14 @@ const ItemListSection = () => {
         <LayoutSwitch onChangeLayout={onChangeLayout} renderedLayout={itemsLayout} />
       </div>
       <div className={`${styles['itemlist-container']} ${itemsLayout === PRODUCT_ITEMS_LAYOUT_GRID ? styles['grid-view'] : styles['list-view']}`}>
-        <GridProductItem />
+        {(!isLoading || data.length !== 0 ? data : DUMMY_LOADING_ARRAY).map((item) => {
+          if (isLoading) return (
+            <div className={styles['itemlist-loader']}>
+              <LoadingPlaceholder containerClassName={styles['loader']} style={{ height: '100%' }} />
+            </div>
+          )
+          return <GridProductItem />;
+        })}
       </div>
       
       <FloatingButton className={styles['floating-add-button']} onClick={redirectToInputPage}>
