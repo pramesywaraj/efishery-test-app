04c936e667e74f73803812119ab16e3c1dd3edbb
pramesywaraj <pramesywaraj@pahamify.com>
2022-08-16 17:04:48 +0700

    feat: add float button to redirect to add item
    add floating button in the bottom of the screen to redirect to
    input page screen

--- src/pages/HomePage/partials/ItemListSection/index.jsx
+++ src/pages/HomePage/partials/ItemListSection/index.jsx
@@ -1,16 +1,21 @@
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 import Search from '@components/Search';
 import { GridProductItem } from '@components/Items';
 import LayoutSwitch from '@components/Screens/HomePage/LayoutSwitch';
+import { FloatingButton } from '@components/Buttons';
 
 import { PRODUCT_ITEMS_LAYOUT_GRID } from '@constants/product';
+import { ReactComponent as IconAdd } from '@assets/images/icons/icon-add.svg';
 
 import styles from './styles.module.scss';
 
 const ItemListSection = () => {
   const [searchTerm, setSearchTerm] = useState('');
   const [itemsLayout, setItemsLayout] = useState(PRODUCT_ITEMS_LAYOUT_GRID);
+  
+  const navigation = useNavigate();
   
   const onChangeSearchText = (event) => {
     const { value } = event.target;
@@ -27,6 +32,10 @@ const ItemListSection = () => {
     setItemsLayout(selectedLayout);
   };
   
+  const redirectToInputPage = () => {
+    navigation('/input-item');
+  };
+  
   return (
     <section className={styles['itemlist-section']}>
       <Search onChange={onChangeSearchText} searchTerm={searchTerm} onSubmit={onSubmitSearch} />
@@ -36,8 +45,11 @@ const ItemListSection = () => {
       </div>
       <div className={`${styles['itemlist-container']} ${itemsLayout === PRODUCT_ITEMS_LAYOUT_GRID ? styles['grid-view'] : styles['list-view']}`}>
         <GridProductItem />
-        <GridProductItem />
       </div>
+      
+      <FloatingButton className={styles['floating-add-button']} onClick={redirectToInputPage}>
+        <IconAdd />
+      </FloatingButton>
     </section>
   );
 };
