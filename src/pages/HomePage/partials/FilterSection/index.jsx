2fd80368b34c78ec61221b446dccd370c77a9d14
pramesywaraj <pramesywaraj@pahamify.com>
2022-08-16 22:21:01 +0700

    feat: init filter section

--- src/pages/HomePage/partials/FilterSection/index.jsx
+++ src/pages/HomePage/partials/FilterSection/index.jsx
@@ -1,7 +1,16 @@
 import styles from './styles.module.scss';
 
 const FilterSection = () => {
-  return <section className={styles['homepage-filter-section']}>filter section</section>;
+  return (
+    <section className={styles['homepage-filter-section']}>
+      <div className={styles['homepage-filter-card']}>
+        <p>Filter berdasarkan Area</p>
+      </div>
+      <div className={styles['homepage-filter-card']}>
+        <p>Filter berdasarkan Ukuran</p>
+      </div>
+    </section>
+  );
 };
 
 export default FilterSection;
