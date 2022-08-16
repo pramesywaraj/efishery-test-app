403c714930d8006c6a2d47bad553a267c92c5aeb
pramesywaraj <pramesywaraj@pahamify.com>
2022-08-16 20:05:08 +0700

    chore: include needed css files

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from '@/App';
 import '@/global.scss';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'react-loading-skeleton/dist/skeleton.css'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
