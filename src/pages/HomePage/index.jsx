9d21144a62083af693f148c3e532b1dc4eee2bba
pramesywaraj <pramesywaraj@pahamify.com>
2022-08-16 23:50:48 +0700

    feat: handle city and size filter

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,4 +1,6 @@
-import useDataHandler from '@/hooks/useDataHandler';
+import { useState, useEffect } from 'react';
+import useDataHandler from '@hooks/useDataHandler';
+import useFetch from '@hooks/useFetch';
 
 import ItemListSection from '@pages/HomePage/partials/ItemListSection';
 import FilterSection from '@pages/HomePage/partials/FilterSection';
@@ -7,6 +9,67 @@ import styles from './styles.module.scss';
 
 const HomePage = () => {
   const { isLoading, data, onSearchByTitle } = useDataHandler('/list');
+  const { isLoading: isSizeFiltersLoading, data: dataSizeFilters } = useFetch('/option_size');
+  const { isLoading: isAreaFiltersLoading, data: dataAreaFilters } = useFetch('/option_area');
+  
+  const [sizeOptions, setSizeOptions] = useState(dataSizeFilters);
+  const [areaOptions, setAreaOptions] = useState(dataAreaFilters);
+  const [processedData, setProcessedData] = useState(data);
+  const [areaSelected, setAreaSelected] = useState(null);
+  const [sizeSelected, setSizeSelected] = useState(null);
+  
+  useEffect(() => {
+    if (dataSizeFilters.length > 0) setSizeOptions(dataSizeFilters);
+  }, [dataSizeFilters]);
+  
+  useEffect(() => {
+    if (dataAreaFilters.length > 0) setAreaOptions(dataAreaFilters);
+  }, [dataAreaFilters]);
+  
+  useEffect(() => {
+    setProcessedData(data);
+  }, [data]);
+  
+  const onChangeOptionArea = (e) => {
+    const selectedValue = e.target.value;
+    let clonedData = [...processedData];
+    
+    if (selectedValue !== areaSelected) {
+      clonedData = [...data];
+
+      if (selectedValue === 'NONE') {
+        setProcessedData(data);
+        setAreaSelected(null);
+        return;
+      };
+    }
+
+    const filteredData = clonedData.filter((item) => item.area_kota === selectedValue.toUpperCase());
+    
+    setAreaSelected(e.target.value);
+    setProcessedData(filteredData);
+  }
+  
+  const onChangeOptionSize = (e) => {
+    const selectedValue = e.target.value;
+    let clonedData = [...processedData];
+    
+    if (selectedValue !== sizeSelected) {
+      clonedData = [...data];
+
+      if (selectedValue === 'NONE') {
+        setProcessedData(data);
+        setSizeSelected(null);
+        return;
+      };
+    }
+
+
+    const filteredData = clonedData.filter((item) => item.size === selectedValue);
+    
+    setSizeSelected(e.target.value);
+    setProcessedData(filteredData);
+  }
 
   return (
     <main className={styles['homepage-main']}>
@@ -14,8 +77,35 @@ const HomePage = () => {
         <h2>Daftar Produk Perikanan</h2>
       </header>
       <section className={styles['homepage-section-container']}>
-        <FilterSection />
-        <ItemListSection isLoading={isLoading} products={data} onSearchByTitle={onSearchByTitle} />
+        <FilterSection
+          className={styles['filter-desktop']}
+          sizeSelected={sizeSelected}
+          sizeOptions={sizeOptions}
+          isLoadingSize={isSizeFiltersLoading}
+          areaSelected={areaSelected}
+          areaOptions={areaOptions}
+          isLoadingArea={isAreaFiltersLoading}
+          onChangeOptionArea={onChangeOptionArea}
+          onChangeOptionSize={onChangeOptionSize}
+        />
+        <ItemListSection
+          isLoading={isLoading}
+          products={processedData}
+          onSearchByTitle={onSearchByTitle}
+          filter={
+            <FilterSection
+              className={styles['filter-mobile']}
+              sizeSelected={sizeSelected}
+              sizeOptions={sizeOptions}
+              isLoadingSize={isSizeFiltersLoading}
+              areaSelected={areaSelected}
+              areaOptions={areaOptions}
+              isLoadingArea={isAreaFiltersLoading}
+              onChangeOptionArea={onChangeOptionArea}
+              onChangeOptionSize={onChangeOptionSize}
+            />
+          }
+        />
       </section>
     </main>
   );
--- src/pages/HomePage/partials/FilterSection/index.jsx
+++ src/pages/HomePage/partials/FilterSection/index.jsx
@@ -1,13 +1,53 @@
+import { useMemo } from 'react';
+import { SelectOption } from '@components/Inputs';
+import { LoadingPlaceholder } from '@components/Loaders';
+import { formatStringToPascal } from '@helpers/style';
 import styles from './styles.module.scss';
 
-const FilterSection = () => {
+const FilterSection = ({ areaSelected, sizeSelected, sizeOptions, areaOptions, isLoadingSize, isLoadingArea, onChangeOptionArea, onChangeOptionSize, className }) => {
+  const parsedAreaOptions = useMemo(() => {
+    if (!areaOptions || areaOptions.length === 0) return [];
+    const tempOptions = [...areaOptions];
+    let mappedOptions = tempOptions.map((item) => {
+      return {
+        value: item.city,
+        label: formatStringToPascal(item.city)
+      }
+    });
+    mappedOptions.unshift({ value: 'NONE', label: 'Pilih area...' });
+
+    return mappedOptions;
+  }, [areaOptions]);
+
+  const parsedSizeOptions = useMemo(() => {
+    if (!sizeOptions || sizeOptions.length === 0) return [];
+
+    const tempOptions = [...sizeOptions];
+    let mappedOptions = tempOptions.map((item) => {
+      return {
+        value: item.size,
+        label: item.size
+      }
+    });
+    
+    mappedOptions.unshift({ value: 'NONE', label: 'Pilih ukuran...' });
+    
+    return mappedOptions;
+  }, [sizeOptions]);
+
   return (
-    <section className={styles['homepage-filter-section']}>
+    <section className={`${styles['homepage-filter-section']} ${className}`}>
       <div className={styles['homepage-filter-card']}>
         <p>Filter berdasarkan Area</p>
+        {!isLoadingSize ? <SelectOption selectedValue={areaSelected} options={parsedAreaOptions} onChange={onChangeOptionArea} /> : (
+          <LoadingPlaceholder />
+        )}
       </div>
       <div className={styles['homepage-filter-card']}>
         <p>Filter berdasarkan Ukuran</p>
+        {!isLoadingArea ? <SelectOption selectedValue={sizeSelected}  options={parsedSizeOptions} onChange={onChangeOptionSize} /> : (
+          <LoadingPlaceholder />
+        )}
       </div>
     </section>
   );
